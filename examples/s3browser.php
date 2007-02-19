81e1794d0acbe4fcd730dd4fcd9ddfa5bca8d39c
Tyler Hall <tylerhall@gmail.com>
2007-02-19 17:03:41 +0000

    Improved S3 browser.

--- examples/s3browser.php
+++ examples/s3browser.php
@@ -36,6 +36,8 @@
 
 	$path = trim($_SESSION['PATH'], "/");
 	$_SESSION['PATH'] = $path;
+
+	$buckets = $s3->getBuckets();
 	
 	if(isset($_GET['delete']) && !empty($_GET['delete']))
 		$s3->recursiveDelete($bucket, $_GET['delete']);
@@ -43,12 +45,12 @@
 	function human_readable($val, $round = 0)
 	{
 		$unit = array('','K','M','G','T','P','E','Z','Y');
-		while($val >= 1000)
+		while($val >= 1024)
 		{
 			$val /= 1024;
 			array_shift($unit);
 		}
-		return round($val, $round) . array_shift($unit) . "B";
+		return round($val, 2) . array_shift($unit) . "B";
 	}
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
@@ -59,59 +61,80 @@
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
 	<title>S3 File Browser</title>
 	<style type="text/css" media="screen">
+		body { font-family:Verdana, Arial, "sans seif"; font-size:10px; }
 		a { color:#00f; }
-		#logout { position:absolute; top:0; right:0; margin:5px; }
-		#main h1 a { color:#000; text-decoration:underline; }
-		#main pre a { text-decoration:none; }
-		#main pre a:hover { text-decoration:underline; }
-		#main pre a.prefix { font-weight:bold; }
+		#top { position:absolute; top:0; right:0; margin:5px; }
+		#main table { border-collapse:collapse; }
+		#main table th { text-align:left; padding:5px 20px 5px 0; }
+		#main table tr.row1 { background-color:#f1f5fa; }
+		#main table td { padding:5px 20px 5px 0; }
+		#main a { text-decoration:none; color:#000; }
+		#main a:hover { text-decoration:underline; }
+		#main a.prefix { font-weight:bold; }
 	</style>
 </head>
 
 <body>
-	<div id="logout">
-		<a href='<?PHP echo $me;?>?bucket='>Change Buckets</a>
+	<form id="top" acition="" method="get">
+		<select name="bucket" id="bucket">
+			<option value="">Choose a bucket</option>
+			<?PHP
+				foreach($buckets as $b)
+				{
+					if($bucket == $b)
+						echo "<option value='$b' selected='selected'>$b</option>";
+					else
+						echo "<option value='$b'>$b</option>";
+				}
+			?>
+		</select>
+		<input type="submit" name="btnBucket" value="Go" id="btnBucket">
 		<a href='<?PHP echo $me;?>?logout'>Logout</a>
-	</div>
+	</form>
 	<div id="main">
 		<?PHP
-			if(empty($bucket))
+			if(!empty($bucket))
 			{
-				$buckets = $s3->getBuckets();
-				echo "<h1>Choose a Bucket</h1><ul>";
-				foreach($buckets as $name)
-					echo "<li><a href='$me?bucket=$name'>$name</a></li>";
-				echo "</ul>";
-			}
-			else
-			{
-				echo "<h1>Browsing <a href='$me?path=/'>$bucket</a></h1>";
-				
-				$uppath = str_replace(array_pop(explode("/", trim($path, "/"))), "", trim($path, "/"));
-				echo "<h2><a href='$me?path=$uppath'>$path/</a></h2>";
+				echo "<h3>";
+				echo "/<a href='$me?path='>$bucket</a>";
+				foreach(explode("/", $path) as $x)
+				{
+					$y .= "/$x";
+					echo "/<a href='$me?path=$y'>$x</a>";
+				}				
+				echo "</h3>";
 
 				$items = $s3->getBucketContents($bucket, str_replace("//", "/", "/$path/"), "/");
 				$items = $s3->sortKeys($items);
 
 				echo "<p>" . count($items) . " items</p>";
 			
-				echo "<pre>";
+				echo "<table>";
+				echo "<tr><th>Size</th><th>Name</th><th>&nbsp;</th></tr>";
 				foreach($items as $item)
 				{
+					$row = ($row == "1") ? "2" : "1";
+					echo "<tr class='row$row'>";
 					if($item["type"] == "key")
 					{
 						$link = "http://s3.amazonaws.com/$bucket/" . $item["name"];
 						$name = str_replace(trim($path, "/") . "/", "", $item["name"]);
-						printf("%s %-10s %s\n", "<a href='$me?delete={$item['name']}' onclick='return confirm(\"Are you sure you want to delete this item?\");'>[x]</a>", human_readable($item["size"], 2), "<a href='$link' class='key'>$name</a>");
+
+						echo "<td>" . human_readable($item["size"], 2) . "</td>";
+						echo "<td><a href='$link' class='key'>$name</a></td>";
+						echo "<td><a href='$me?delete={$item['name']}' onclick='return confirm(\"Are you sure you want to delete this item?\");'>delete</td>";
 					}
 					else
 					{
 						$link = "$me?path=" . $item['name'];
 						$name = trim(str_replace($path, "", $item['name']), "/") . "/";
-						printf("%s %10s %s\n", "<a href='$me?delete={$item['name']}' onclick='return confirm(\"Are you sure you want to delete this item?\");'>[x]</a>", " ", "<a href='$link' class='prefix'>$name</a>");
+						echo "<td>&nbsp;</td>";
+						echo "<td><a href='$link' class='prefix'>$name</a></td>";
+						echo "<td><a href='$me?delete={$item['name']}' onclick='return confirm(\"Are you sure you want to delete this item?\");'>delete</td>";
 					}
+					echo "</tr>";
 				}
-				echo "</pre>";
+				echo "</table>";
 			}
 		?>
 	</div>
